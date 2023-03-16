f116d8a4f7a65acf63b72264c7e0a02e7b8e89d1
dariomonopoli-dev <jerryscout71@gmail.com>
2023-03-16 16:50:06 +0100

    added favicon and header title

--- src/components/views/Header.tsx
+++ src/components/views/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ height }) => (
   <div className="header container" style={{ height: `${height}px` }}>
-    <h1 className="header title">SoPra FS23 rocks with React!</h1>
+    <h1 className="header title">GuessTheCountry</h1>
   </div>
 );
 
