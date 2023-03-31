383e2df3d794c0166f015a14af4d289874dbd52c
L-Huber <71383238+L-Huber@users.noreply.github.com>
2023-03-31 19:44:18 +0200

    lobby finder finally working
    need to define attributes in next commit

--- src/components/views/GameLobbyOverview.tsx
+++ src/components/views/GameLobbyOverview.tsx
@@ -37,13 +37,7 @@ function createData(
     return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
 
 
 const GameLobbyOverview: React.FC = () => {
@@ -59,7 +53,8 @@ const GameLobbyOverview: React.FC = () => {
     const [nationality, setNationality] = useState<string | null>(null);
     const [gamesWon, setGamesWon] = useState<number | null>(null);
     const [allCountries, setAllCountries] = useState<Array<string>>([]);
-    const [allLobbies, setAllLobbies] = useState<Array<string>>([]);
+    //if new attribute is need from allLobbies: define it like gameId
+    const [allLobbies, setAllLobbies] = useState<Array<{gameId: any}>>([]);
 
 
     // Save user changes
@@ -190,6 +185,9 @@ const GameLobbyOverview: React.FC = () => {
         fetchCountries();
     }, []);
     useEffect(() => {
+        console.log(allLobbies);
+    }, [allLobbies]);
+    useEffect(() => {
         async function fetchLobbies() {
             try {
                 console.log("started fetching all games")
@@ -222,31 +220,27 @@ const GameLobbyOverview: React.FC = () => {
         return (
             <div>
                 <Typography>Hello</Typography>
-                <div><pre>{JSON.stringify(allLobbies, null, 2) }</pre></div>;
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>Dessert (100g serving)</TableCell>
-                                <TableCell align="right">Calories</TableCell>
-                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                                <TableCell>GameId</TableCell>
+                                <TableCell align="right">Creator</TableCell>
+                                <TableCell align="right">Join</TableCell>
+
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.map((row) => (
+                            {allLobbies.map((lobby, key) => (
                                 <TableRow
-                                    key={row.name}
+                                    key={key}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
                                     <TableCell component="th" scope="row">
-                                        {row.name}
+                                        {lobby?.gameId}
                                     </TableCell>
-                                    <TableCell align="right">{row.calories}</TableCell>
-                                    <TableCell align="right">{row.fat}</TableCell>
-                                    <TableCell align="right">{row.carbs}</TableCell>
-                                    <TableCell align="right">{row.protein}</TableCell>
+                                    <TableCell align="right">{lobby?.gameId}</TableCell>
+                                    <TableCell align="right">{lobby?.gameId}</TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
