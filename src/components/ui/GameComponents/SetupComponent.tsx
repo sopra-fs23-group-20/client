ef9bd94479de65d10f01fdb0338559bd59acbeaa
robonder <jonasblum5@gmail.com>
2023-04-05 13:42:21 +0200

    Changes together with Dario for Game Setup and Settings

--- src/components/ui/GameComponents/SetupComponent.tsx
+++ src/components/ui/GameComponents/SetupComponent.tsx
@@ -1,154 +1,39 @@
-import { useState } from "react";
-import {
-  Container,
-  Typography,
-  Box,
-  FormControlLabel,
-  FormGroup,
-  Checkbox,
-} from "@mui/material";
-import * as React from "react";
-import { Switch } from "@mui/material";
-
-import { TextField, DialogContent, FormControl } from "@mui/material";
+import { useEffect, useState } from "react";
+import { api, handleError } from "helpers/api";
+import { Button, Container, Typography, Box, Grid } from "@mui/material";
+import User from "models/User";
+import { AxiosError } from "axios";
+import Country from "models/Country";
+import Autocomplete from "@mui/material/Autocomplete";
+import { TextField } from "@mui/material";
+import React, { useMemo } from "react";
+import HintComponent from "../HintComponent";
 
 interface Props {
   gameId: string | undefined;
 }
-
 const SetupComponent: React.FC<Props> = (props) => {
   const gameId = props.gameId;
-  const [roundSeconds] = useState(30);
-  const [randomizedHints] = useState(false);
-  const [allCountries] = useState(false);
-  const [numberOfRounds] = useState(1);
-  const [openLobby] = useState(false);
-  const [selectedHints] = useState({
-    population: false,
-    outline: false,
-    flag: false,
-    location: false,
-    capital: false,
-  });
+
+  async function startGame(): Promise<void> {
+    try {
+      await api.put(`/games/${gameId}/start`);
+    } catch (error: AxiosError | any) {
+      alert(
+        `Something went wrong while starting the game: \n${handleError(error)}`
+      );
+    }
+  }
 
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Typography variant="h2">Game Settings</Typography>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-around",
-          marginTop: "1rem",
-        }}
-      ></Box>
-      <FormControl>
-        <DialogContent>
-          <FormControl sx={{ minWidth: "200px", marginBottom: "1rem" }}>
-            <TextField
-              id="round-seconds"
-              label="Round Seconds"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={roundSeconds}
-              disabled={true}
-            />
-          </FormControl>
-
-          <FormGroup>
-            <TextField
-              id="number-of-rounds"
-              label="Number of Rounds"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={numberOfRounds}
-              disabled={true}
-            />
-            <FormControlLabel
-              control={<Checkbox checked={randomizedHints} disabled={true} />}
-              label="Randomized Hints"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={allCountries} disabled={true} />}
-              label="All Countries"
-            />
-            <FormControlLabel
-              control={
-                <Switch checked={openLobby} disabled={true} color="primary" />
-              }
-              label="Open Lobby"
-            />
-          </FormGroup>
-
-          <FormControl component="fieldset" sx={{ marginTop: "1rem" }}>
-            <Typography variant="subtitle1">Selected Hints:</Typography>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectedHints.population}
-                    disabled={true}
-                    name="population"
-                  />
-                }
-                label="Population"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectedHints.outline}
-                    disabled={true}
-                    name="outline"
-                  />
-                }
-                label="Outline"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectedHints.flag}
-                    disabled={true}
-                    name="flag"
-                  />
-                }
-                label="Flag"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectedHints.location}
-                    disabled={true}
-                    name="location"
-                  />
-                }
-                label="Location"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectedHints.capital}
-                    disabled={true}
-                    name="capital"
-                  />
-                }
-                label="Capital"
-              />
-            </FormGroup>
-          </FormControl>
-        </DialogContent>
-      </FormControl>
+    <Container>
+      <Typography variant="h2">You are now in a Game!</Typography>
+      <Box sx={{ textAlign: "center", marginTop: 1 }}>
+        <Button variant="outlined" onClick={() => startGame()}>
+          Start Game
+        </Button>
+      </Box>
     </Container>
   );
 };
-
 export default SetupComponent;
