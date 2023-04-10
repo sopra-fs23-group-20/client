b7c8a0689f72cb9db7bbf19f631a266aae7bd935
robonder <jonasblum5@gmail.com>
2023-04-10 19:54:30 +0200

    Removed one import

--- src/components/ui/GameComponents/EndedComponent.tsx
+++ src/components/ui/GameComponents/EndedComponent.tsx
@@ -1,7 +1,6 @@
 import { Button, Container, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { makeStyles } from "@mui/styles";
 
 interface Props {}
 
