69779846f7d38cb39bb23d7f2a3ee30d1cba9236
robonder <jonasblum5@gmail.com>
2023-04-26 16:38:31 +0200

    Merge conflict resolved

--- src/components/ui/GameComponents/GuessingComponent.tsx
+++ src/components/ui/GameComponents/GuessingComponent.tsx
@@ -7,12 +7,6 @@ import {
   Divider,
   TextField,
   Typography,
-  Box,
-  Button,
-  Container,
-  Divider,
-  TextField,
-  Typography,
 } from "@mui/material";
 import { AxiosError } from "axios";
 import Autocomplete from "@mui/material/Autocomplete";
@@ -24,9 +18,6 @@ interface Props {
   gameGetDTO: GameGetDTO | null;
   allCountries: Array<string>;
   currentUserId: string | null;
-  gameGetDTO: GameGetDTO | null;
-  allCountries: Array<string>;
-  currentUserId: string | null;
 }
 
 const GuessingComponent: React.FC<Props> = (props) => {
@@ -34,11 +25,7 @@ const GuessingComponent: React.FC<Props> = (props) => {
   const game = props.gameGetDTO;
   const currentUserId = props.currentUserId;
   const { showAlert } = useAlert();
-  const allCountries = props.allCountries;
-  const game = props.gameGetDTO;
-  const currentUserId = props.currentUserId;
 
-  const [valueEntered, setValueEntered] = useState<string | null>(null);
   const [valueEntered, setValueEntered] = useState<string | null>(null);
 
   async function submitGuess(): Promise<void> {
@@ -56,19 +43,6 @@ const GuessingComponent: React.FC<Props> = (props) => {
       }
     } catch (error: AxiosError | any) {
       showAlert(error.response.data.message, "error");
-    }
-  }
-  async function submitGuess(): Promise<void> {
-    try {
-      console.log("Submitting guess", valueEntered);
-      const request = await api.post(`/games/${game?.gameId}/guesses`, {
-        userId: currentUserId,
-        guess: valueEntered,
-      });
-      const requestBody = request.data;
-      alert(requestBody);
-    } catch (error: AxiosError | any) {
-      alert(error.response.data.message);
     }
   }
 
@@ -167,94 +141,10 @@ const GuessingComponent: React.FC<Props> = (props) => {
         ) : (
           <div></div>
         )}
-        return (
-        <Container
-          sx={{
-            marginTop: "10vh",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              marginTop: "5%",
-              marginBottom: "5%",
-              width: "100%",
-              height: "200%",
-            }}
-          >
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={allCountries}
-              sx={{ width: "90%", height: "200%" }}
-              onChange={(event, value) => setValueEntered(value)}
-              renderInput={(params) => (
-                <TextField {...params} label="Enter your Guess here" />
-              )}
-            />
-            <Button
-              variant="outlined"
-              sx={{ marginLeft: "2%", height: "100%" }}
-              onClick={() => submitGuess()}
-              disabled={hasPlayerGuessed()}
-            >
-              Submit
-            </Button>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h3">
-              Round:{" "}
-              {game?.numberOfRounds != null && game?.remainingRounds != null
-                ? game.numberOfRounds -
-                  game.remainingRounds +
-                  "/" +
-                  game.numberOfRounds
-                : "undefined"}
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              marginTop: "5%",
-              justifyContent: "space-between",
-            }}
-          >
-            {game?.remainingTime ? (
-              <Typography variant="h4">
-                Time Left: {game.remainingTime.toString()}{" "}
-              </Typography>
-            ) : (
-              <div></div>
-            )}
 
-            {game?.remainingRoundPoints ? (
-              <Typography variant="h4">
-                Achievable Points: {game.remainingRoundPoints.toString()}{" "}
-              </Typography>
-            ) : (
-              <div></div>
-            )}
-          </Box>
-          <Divider sx={{ marginTop: "5%" }}> Current Hint:</Divider>
-          <Box sx={{ height: "50%", width: "100%", marginTop: "5%" }}>
-            <HintContainer
-              currentCaregory={game?.categoryStack?.currentCategory}
-            />
-          </Box>
-        </Container>
-        );
         {game?.remainingRoundPoints ? (
           <Typography variant="h4">
-            Points Left: {game.remainingRoundPoints.toString()}{" "}
+            Achievable Points: {game.remainingRoundPoints.toString()}{" "}
           </Typography>
         ) : (
           <div></div>
