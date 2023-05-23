af0185b316562ada9233bc3db4f67784cfc5e4e9
L-Huber <71383238+L-Huber@users.noreply.github.com>
2023-05-23 16:31:00 +0200

    Update Landing.tsx
    -deleted green circle

--- src/components/views/Landing.tsx
+++ src/components/views/Landing.tsx
@@ -140,19 +140,6 @@ export default function Landing() {
                           >
                               Login
                           </Button>
-
-                          <div
-                              id="green-circle"
-                              style={{
-                                  borderRadius: "50%",
-                                  width: "90vh",
-                                  height: "90vh",
-                                  background: "#5EB23F",
-                                  position: "absolute",
-                                  top: 0,
-                                  right: -300,
-                              }}
-                          ></div>
                           <div id="mockup">
                               <img
                                   src={Mockup}
